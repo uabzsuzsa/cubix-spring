a9416748ec857dbfa0cb503240f9c85202644e1d
uabzsuzsa <uabzsuzsa@gmail.com>
2023-11-02 14:53:52 +0100

    rest api példa

--- airport/src/main/java/hu/cubix/airport/controller/AirportController.java
+++ airport/src/main/java/hu/cubix/airport/controller/AirportController.java
@@ -1,71 +1,95 @@
 package hu.cubix.airport.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import hu.cubix.airport.dto.AirportDTO;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
+/**
+ * api: rest api url -ek ide kerülnek
+ * localhost:8080/api/airports
+ * RequestMapping("/api/airports") : az összes GET,POST,..stb hívás, ami az osztályon
+ * belül íródik, ez alá az url alá megy, nem kell külön jelölni.
+ * @author Zsuzsa
+ *
+ */
+@RestController
+@RequestMapping("/api/airports")
 public class AirportController {
 	
-	private List<AirportDTO> airports = new ArrayList<>();
+	private Map<Long,AirportDTO> airports = new HashMap<>();
 	{
-		airports.add(new AirportDTO(11, "Budapest Airport", "BUD"));
+		airports.put(11L,new AirportDTO(11, "Budapest Airport", "BUD"));
 	}
 	
-	@GetMapping("/")
-	public String home(Map<String, Object> model) {
-		model.put("airports", airports);
-		model.put("newAirport", new AirportDTO());
-		System.out.println("model: "+model.get("airports"));
-		return "index";
+	@GetMapping
+	public List<AirportDTO> findAll() {
+		//return airports.values().stream().toList();		
+		return new ArrayList<>(airports.values());
 	}
 	
-	@PostMapping("/airport")
-	public String createAirport(AirportDTO airport, Map<String, Object> model) {
-		airports.add(airport);
-		model.put("airports", airports);
-		System.out.println("airport: "+airport);
-		model.put("newAirport", new AirportDTO());
-		// a browser-ből egy GET hívás fog jönni a home '/' url-re (http://localhost:8080/)
-		return "redirect:/";
-	}
-	
-	
-	/**
-	 * A kód redundáns, ezért a home() metódust hívjuk meg
-	 * @param airport
-	 * @param model
-	 * @return
-	 */
-//	@PostMapping("/airport")
-//	public String createAirport(AirportDTO airport, Map<String, Object> model) {
-//		airports.add(airport);
-//		model.put("airports", airports);
-//		System.out.println("airport: "+airport);
-//		model.put("newAirport", new AirportDTO());
-//		return "index";
+//	@PostMapping
+//	public AirportDTO create(@RequestBody AirportDTO airport) {
+//		airports.put(airport.getId(), airport);
+//		return airport;
+//	}
+//	
+//	@GetMapping("/{id}")
+//	public AirportDTO findById(@PathVariable long id) {
+//		return airports.get(id);
 //	}
 	
+	@PostMapping
+	public ResponseEntity<AirportDTO> create(@RequestBody AirportDTO airport) {
+		
+		if ( airports.containsKey(airport.getId()) ) {
+			return ResponseEntity.badRequest().build();
+		}
+		airports.put(airport.getId(), airport);
+		return ResponseEntity.ok(airport);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<AirportDTO> update(@PathVariable long id, @RequestBody AirportDTO airport) {
+		// a paraméterben küldött id lesz az erősebb
+		airport.setId(id);
+		if ( !airports.containsKey(id) ) {
+			return ResponseEntity.notFound().build();
+		}
+		airports.put(id, airport);
+		return ResponseEntity.ok(airport);
+	}
+	
 	/**
-	 * a metódus végén mindig meghívja a home() metódust, de ebben az esetben
-	 * az /airport mindig az utolsó beírt adattal fog visszajönni, ha a browser-ben     F5 -öt nyomunk
-	 * @param airport
-	 * @param model
+	 * Ha nem talál adatot, akkor a 404 -es hibával tér vissza
+	 * Ezt a ResponseEntity osztály tudja megcsinálni
+	 * @param id
 	 * @return
 	 */
-//	@PostMapping("/airport")
-//	public String createAirport(AirportDTO airport, Map<String, Object> model) {
-//		airports.add(airport);
-//		model.put("airports", airports);
-//		System.out.println("airport: "+airport);
-//		model.put("newAirport", new AirportDTO());
-//		return home(model);
-//	}
+	@GetMapping("/{id}")
+	public ResponseEntity<AirportDTO> findById(@PathVariable long id) {
+		AirportDTO airportDTO = airports.get(id);
+		if ( airportDTO == null ) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(airportDTO);
+	}
 
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		airports.remove(id);
+	}
 }
---
+++ airport/src/main/java/hu/cubix/airport/controller/AirportTLController.java
@@ -0,0 +1,76 @@
+package hu.cubix.airport.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import hu.cubix.airport.dto.AirportDTO;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * TL: ThymeLeaf
+ * @author Zsuzsa
+ *
+ */
+@Controller
+public class AirportTLController {
+	
+	private List<AirportDTO> airports = new ArrayList<>();
+	{
+		airports.add(new AirportDTO(11, "Budapest Airport", "BUD"));
+	}
+	
+	@GetMapping("/")
+	public String home(Map<String, Object> model) {
+		model.put("airports", airports);
+		model.put("newAirport", new AirportDTO());
+		System.out.println("model: "+model.get("airports"));
+		return "index";
+	}
+	
+	@PostMapping("/airport")
+	public String createAirport(AirportDTO airport, Map<String, Object> model) {
+		airports.add(airport);
+		model.put("airports", airports);
+		System.out.println("airport: "+airport);
+		model.put("newAirport", new AirportDTO());
+		// a browser-ből egy GET hívás fog jönni a home '/' url-re (http://localhost:8080/)
+		return "redirect:/";
+	}
+	
+	
+	/**
+	 * A kód redundáns, ezért a home() metódust hívjuk meg
+	 * @param airport
+	 * @param model
+	 * @return
+	 */
+//	@PostMapping("/airport")
+//	public String createAirport(AirportDTO airport, Map<String, Object> model) {
+//		airports.add(airport);
+//		model.put("airports", airports);
+//		System.out.println("airport: "+airport);
+//		model.put("newAirport", new AirportDTO());
+//		return "index";
+//	}
+	
+	/**
+	 * a metódus végén mindig meghívja a home() metódust, de ebben az esetben
+	 * az /airport mindig az utolsó beírt adattal fog visszajönni, ha a browser-ben     F5 -öt nyomunk
+	 * @param airport
+	 * @param model
+	 * @return
+	 */
+//	@PostMapping("/airport")
+//	public String createAirport(AirportDTO airport, Map<String, Object> model) {
+//		airports.add(airport);
+//		model.put("airports", airports);
+//		System.out.println("airport: "+airport);
+//		model.put("newAirport", new AirportDTO());
+//		return home(model);
+//	}
+
+}
